a24bd5a33eeb50c4ff1a9ca882e984dbca3af1e8
jie786389209 <786389209@qq.com>
2018-01-10 23:17:50 +0800

    Create Intersection_of_Two_Linked_Lists.cpp

---
+++ Linked List/Intersection_of_Two_Linked_Lists.cpp
@@ -0,0 +1,56 @@
+///方法一，不用知道len
+/// 6--1--2--3--4  2--5--3--4   相同3--4
+/// p1             p2
+
+/// 6--1--2--3--4  2--5--3--4   相同3--4
+///    p2          p1
+
+///上图可知p2和p1指向list的最终长度一致
+
+
+
+public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    //boundary check
+    if(headA == null || headB == null) return null;
+    
+    ListNode a = headA;
+    ListNode b = headB;
+    
+    //if a & b have different len, then we will stop the loop after second iteration
+    while( a != b){
+    	//for the end of first iteration, we just reset the pointer to the head of another linkedlist
+        a = a == null? headB : a.next;
+        b = b == null? headA : b.next;    
+    }
+    
+    return a;
+}
+
+///方法二,知道len
+public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    int lenA = length(headA), lenB = length(headB);
+    // move headA and headB to the same start point
+    while (lenA > lenB) {
+        headA = headA.next;
+        lenA--;
+    }
+    while (lenA < lenB) {
+        headB = headB.next;
+        lenB--;
+    }
+    // find the intersection until end
+    while (headA != headB) {
+        headA = headA.next;
+        headB = headB.next;
+    }
+    return headA;
+}
+
+private int length(ListNode node) {
+    int length = 0;
+    while (node != null) {
+        node = node.next;
+        length++;
+    }
+    return length;
+}
