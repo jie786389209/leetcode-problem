0f8320b20dc1017028a52f9c7ab28951ffe0cc3d
jie786389209 <786389209@qq.com>
2018-01-11 20:24:25 +0800

    Create Move_Zeroes.cpp

---
+++ array/Move_Zeroes.cpp
@@ -0,0 +1,17 @@
+
+/////思路：i和j指针，i遍历当前，j遇到零就停等待赋值前移
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        // move all the nonzero elements advance
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (;j < nums.size(); j++) {
+            nums[j] = 0;
+        }
+    }
+};
