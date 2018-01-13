529a9c9f2aa2b1a13740166ebc2d22c653be3eb9
jie786389209 <786389209@qq.com>
2018-01-13 22:21:54 +0800

    Create Two_Sum.cpp

---
+++ array/Two_Sum.cpp
@@ -0,0 +1,32 @@
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+////////////////////////////////
+思路：只需要遍历一次，如果不存在就插入，存在则检验结果
+遍历两次的思想是先遍历一次插入map，再遍历一次去验证
+下面是遍历一次的方法
+vector<int> twoSum(vector<int> &numbers, int target)
+{
+    //Key is the number and value is its index in the vector.
+	unordered_map<int, int> hash;
+	vector<int> result;
+	for (int i = 0; i < numbers.size(); i++) {
+		int numberToFind = target - numbers[i];
+
+            //if numberToFind is found in map, return them
+		if (hash.find(numberToFind) != hash.end()) {
+                    //+1 because indices are NOT zero based
+			result.push_back(hash[numberToFind] + 1); //返回的是idex+1，即第几个
+			result.push_back(i + 1);			
+			return result;
+		}
+
+            //number was not found. Put it in the map.
+		hash[numbers[i]] = i;
+	}
+	return result;
+}
