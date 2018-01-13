b957e3a1c8a74c30372c0d8a4961e5da4ba62b7d
jie786389209 <786389209@qq.com>
2018-01-13 19:31:41 +0800

    Create Maximum_Subarray.cpp

---
+++ array/Maximum_Subarray.cpp
@@ -0,0 +1,18 @@
+Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+///////
+class Solution {
+public:
+    int maxSubArray(int A[], int n) {
+        int ans=A[0],i,j,sum=0;
+        for(i=0;i<n;i++){
+            sum+=A[i];
+            ans=max(sum,ans);
+            sum=max(sum,0);
+        }
+        return ans;
+    }
+};
