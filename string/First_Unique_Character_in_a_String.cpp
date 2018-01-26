9f428daa568eb35bea4266d7a5773f7e1bcd76b0
jie786389209 <786389209@qq.com>
2018-01-26 10:44:35 +0800

    Create First_Unique_Character_in_a_String.cpp

---
+++ string/First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> m;
+        for (auto &c : s) {
+            m[c]++;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (m[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
+
+/////////////////////////////////////
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, pair<int, int>> m;
+        int idx = s.size();
+        for (int i = 0; i < s.size(); i++) {
+            m[s[i]].first++;
+            m[s[i]].second = i;
+        }
+        for (auto &p : m) {
+            if (p.second.first == 1) idx = min(idx, p.second.second);
+        }
+        return idx == s.size() ? -1 : idx;
+    }
+};
