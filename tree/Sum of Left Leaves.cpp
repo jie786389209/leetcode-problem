04a77f108c79b89a1472bc6a547557a8bdff811c
jie786389209 <786389209@qq.com>
2018-01-30 16:17:57 +0800

    Create Sum of Left Leaves.cpp

---
+++ tree/Sum of Left Leaves.cpp
@@ -0,0 +1,36 @@
+Recursive method. For given node we check whether its left child is a leaf. If it is the case, we add its value to answer, otherwise recursively call method on left child. For right child we call method only if it has at least one nonnull child.
+
+public int sumOfLeftLeaves(TreeNode root) {
+    if(root == null) return 0;
+    int ans = 0;
+    if(root.left != null) {
+        if(root.left.left == null && root.left.right == null) ans += root.left.val;
+        else ans += sumOfLeftLeaves(root.left);
+    }
+    ans += sumOfLeftLeaves(root.right);
+    
+    return ans;
+}
+Iterative method. Here for each node in the tree we check whether its left child is a leaf. If it is true, we add its value to answer, otherwise add left child to the stack to process it later. For right child we add it to stack only if it is not a leaf.
+
+public int sumOfLeftLeaves(TreeNode root) {
+    if(root == null) return 0;
+    int ans = 0;
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+    stack.push(root);
+    
+    while(!stack.empty()) {
+        TreeNode node = stack.pop();
+        if(node.left != null) {
+            if (node.left.left == null && node.left.right == null)
+                ans += node.left.val;
+            else
+                stack.push(node.left);
+        }
+        if(node.right != null) {
+            if (node.right.left != null || node.right.right != null)
+                stack.push(node.right);
+        }
+    }
+    return ans;
+}
