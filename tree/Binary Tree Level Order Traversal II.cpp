1abe4ddfa93216db68923e373a2a32793de6078a
jie786389209 <786389209@qq.com>
2018-01-31 10:03:36 +0800

    Update Binary Tree Level Order Traversal II.cpp

--- tree/Binary Tree Level Order Traversal II.cpp
+++ tree/Binary Tree Level Order Traversal II.cpp
@@ -29,26 +29,16 @@ public:
         while(!q.empty())
         {
             int s=q.size();
-            queue<TreeNode*> q_temp=q;
             level.clear();
-            for(int j=0;j<s;j++) ///在队列中每个level的那些值压进res
-            {
-                TreeNode* aa(q_temp.front());
-                level.push_back(aa->val);
-                q_temp.pop();
-            }
-            res.insert(res.begin(),level);
-            
-            
             for(int j=0;j<s;j++)
             {
                 TreeNode* node=q.front();
+                level.push_back(node->val);
                 q.pop();
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
-            
-            
+            res.insert(res.begin(),level);
         }
         return res;    
     }
